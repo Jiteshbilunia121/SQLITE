0e727dbf6de6bdb3d7663166a92cc98d4bc6a09c
jiteshbilunia121 <jiteshbilunia1234@gmail.com>
2024-09-05 16:43:59 +0530

    Values cached in Hashmap

--- src/main/java/Init/LoadDB.java
+++ src/main/java/Init/LoadDB.java
@@ -1,6 +1,8 @@
 package Init;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -9,7 +11,7 @@ import java.util.HashMap;
 
 public class LoadDB {
 
-    public HashMap<String, HashMap<Integer, String> > map = new HashMap<>();
+    public HashMap<String, HashMap<String, String> > map = new HashMap<>();
     // Key of (outer)hashmap -- table name ..... this table name = file name
     // value of (outer)hashmap -- contents of the table
 
@@ -39,14 +41,29 @@ public class LoadDB {
             System.out.println("Security Exception");
         }
     }
-    public void CopyMap(String fileName){
-         HashMap<Integer, String> tempMap = new HashMap<>();
+    public void CopyMap(String tableName){
+         HashMap<String, String> tempMap = new HashMap<>();
          // Key of tempMap = primary key for the table, int value incremented by one for each row in table
-         //....
+        //....
         // Load the contents of this txt file byte wise and copy it to tempMap
 
         // After copying it to tempMap , move the map to the outer map declared above
+        String filePath = "C:\\Users\\Jitesh\\IdeaProjects\\SQLITE\\src\\main\\java\\DB" + tableName;
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] vals = line.split(" ");
+                for (String values : vals) {
+                    tempMap.put(values, line);
+                }
+                // Check if the row contains the search value
+            }
 
+            // The contenets have been copied to the map
+            map.put(tableName, tempMap);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -69,6 +86,5 @@ public class LoadDB {
             throw new RuntimeException(e);
         }
     }
-
 
 }
